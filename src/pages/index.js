ae337a0b91c35a4a120b3b040098077801bbc880
hg-pyun <phg2491@gmail.com>
2021-02-14 18:12:01 +0900

    fix: SEO Title

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ class BlogIndex extends React.Component {
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
+        <SEO title="The best moment has not coming yet" />
         <Bio />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug;
